a8f15c48f444761feaf8079618c97d64c83353fe
Adela Knap <adkn2300@student.miun.se>
2025-01-23 09:21:44 +0100

    Lägger till konvertering

--- obj/Debug/net9.0/Blazor1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Blazor1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf097550720456718ef0d4fcd3707626e4a2f88b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
